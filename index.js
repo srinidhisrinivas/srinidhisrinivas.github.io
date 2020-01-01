d7671e0402875accc82348de7cbc9f60fef5a0cc
srinidhisrinivas <srinidhisrinivas@hotmail.com>
2019-12-31 20:03:58 -0500

    test

---
+++ index.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+var files = fs.readdirSync('C:/Users/Srinidhi/Documents/writing/*.txt')
+function displayThings(){
+	var allText = document.getElementById('allText');
+	allText.innertext = 'test';
+}
