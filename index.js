76cb778775a2225cc97f62709eac697c1e930d3c
srinidhisrinivas <srinidhisrinivas@hotmail.com>
2019-12-31 20:15:40 -0500

    init

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var fs = require('fs');
 var files = fs.readdirSync('C:/Users/Srinidhi/Documents/writing/*.txt')
 function displayThings(){
 	var allText = document.getElementById('allText');
-	allText.innertext = files.toString();
+	allText.innertext = files;
 }
