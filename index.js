71b0dd0f50a964f5fc8896be17efda3fb34fcc86
srinidhilinux <srinidhisrinivas@hotmail.com>
2024-08-27 14:38:33 +0530

    removed index files

--- index.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs');
-var files = fs.readdirSync('writing/*.txt');
-function displayThings(){
-	var allText = document.getElementById('allText');
-	allText.innertext = 'reach';
-}
